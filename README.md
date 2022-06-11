9d88e60b7df3c87ab67807d1c946db5e7e53acf1
seankzw-ubuntu <seankzw@gmail.com>
2022-06-11 12:53:12 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ayahgen
 # ayahgen
+# ayahgen
--- script.js
+++ script.js
@@ -1,145 +1,130 @@
-const quoteContainer = document.getElementById('quote-container');
-const quoteTextA = document.getElementById('quotea');
-const quoteTextE = document.getElementById('quotee');
-const ayahText = document.getElementById('ayah');
-const twitterBtn = document.getElementById('twitter');
-const newQuoteBtn = document.getElementById('new-quote');
-const loader = document.getElementById('loader');
+const quoteContainer = document.getElementById("quote-container");
+const quoteTextA = document.getElementById("quotea");
+const quoteTextE = document.getElementById("quotee");
+const ayahText = document.getElementById("ayah");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
-
-
-
-let apiQuote = []
+let apiQuote = [];
 
 //show new quote
 
-
-function loading()
-{
-    loader.hidden = false;
-    quoteContainer.hidden = true;
-
+//Show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-function complete()
-{
- quoteContainer.hidden = false;
- loader.hidden = true;
+//Hide loader
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
 }
 
-async function newQuote()
-{
-    loading();
-    verse = Math.floor(Math.random()* 6237)+1;
-    const apiurl = 'https://api.alquran.cloud/ayah/'+verse+'/editions/quran-uthmani,en.pickthall'
+async function newQuote() {
+  loading();
+  verse = Math.floor(Math.random() * 6237) + 1;
+  const apiurl =
+    "https://api.alquran.cloud/ayah/" +
+    verse +
+    "/editions/quran-uthmani,en.pickthall";
 
-    try {
-        const response = await fetch(apiurl)
-        apiQuote = await response.json();
-        console.log(apiQuote);
+  try {
+    let time1 = performance.now();
+    const response = await fetch(apiurl);
+    apiQuote = await response.json();
+    let time2= performance.now();
+    console.log('newQuote() performance:')
+    console.log(time2-time1);
+    console.log(apiQuote);
+  } catch (error) {
+    //catch error
+  }
 
-    } catch (error) {
+  //check quote length to decide style
 
-        //catch error
+  arabic = apiQuote["data"][1]["text"];
+  english = apiQuote["data"][0]["text"];
 
-    }
+  if (english.length > 50) {
+    quoteTextA.classList.add("long-quote");
+    quoteTextE.classList.add("long-quote");
+    console.log("long");
+  } else {
+    quoteTextA.classList.remove("long-quote");
+    quoteTextE.classList.remove("long-quote");
+  }
 
-    //check quote length to decide style
+  quoteTextA.textContent = arabic;
+  quoteTextE.textContent = english;
+ayahText.textContent =
+    apiQuote["data"][0]["surah"]["englishName"] +
+    "(" +
+    apiQuote["data"][0]["surah"]["number"] +
+    ")" +
+    ":" +
+    apiQuote["data"][0]["numberInSurah"];
 
-    arabic = apiQuote['data'][1]['text'];
-    english = apiQuote['data'][0]['text'];
-
-
-
-    if(english.length > 50)
-    {
-        quoteTextA.classList.add('long-quote');
-        quoteTextE.classList.add('long-quote');
-        console.log("long")
-
-    }else {
-        quoteTextA.classList.remove('long-quote');
-        quoteTextE.classList.remove ('long-quote');
-    }
-
-
-
-    quoteTextA.textContent = arabic
-    quoteTextE.textContent = english
-
-    ayahText.textContent = apiQuote['data'][0]['surah']['englishName']+
-    "("+(apiQuote['data'][0]['surah']['number'])+")"+":"+
-    apiQuote['data'][0]['numberInSurah']
-
-    complete();
-
-
+  complete();
 }
 
 //get ayah from api
-async function getayah()
-{
+async function getayah() {
+  loading();
+  let verse = Math.floor(Math.random() * 6237) + 1;
+  const apiurl =
+    "https://api.alquran.cloud/ayah/" +
+    verse +
+    "/editions/quran-uthmani,en.pickthall";
 
-    loading();
-    verse = Math.floor(Math.random()* 6237)+1;
-    const apiurl = 'http://api.alquran.cloud/ayah/'+verse+'/editions/quran-uthmani,en.pickthall'
+  try {
+    let time1 = performance.now(); //! Time Start
+    const response = await fetch(apiurl);
+    apiQuote = await response.json();
+    let time2 = performance.now(); //! Time End
 
-    try {
-        const response = await fetch(apiurl)
-        apiQuote = await response.json();
-        console.log(apiQuote);
+    console.log("getAyah() performance :")
+    console.log(time2-time1) //! Time taken to execute
+    console.log(apiQuote);
+  } catch (error) {
+    //catch error
+  }
 
-    } catch (error) {
+  arabic = apiQuote["data"][1]["text"];
+  english = apiQuote["data"][0]["text"];
 
-        //catch error
+  if (english.length > 50) {
+    quoteTextA.classList.add("long-quote");
+    quoteTextE.classList.add("long-quote");
+    console.log("long");
+  } else {
+    quoteTextA.classList.remove("long-quote");
+    quoteTextE.classList.remove("long-quote");
+  }
 
-    }
+  quoteTextA.textContent = arabic;
+  quoteTextE.textContent = english;
 
-    arabic = apiQuote['data'][1]['text'];
-    english = apiQuote['data'][0]['text'];
+  ayahText.textContent =
+    apiQuote["data"][0]["surah"]["englishName"] +
+    "(" +
+    apiQuote["data"][0]["surah"]["number"] +
+    ")" +
+    ":" +
+    apiQuote["data"][0]["numberInSurah"];
 
-
-
-    if(english.length>50)
-    {
-        quoteTextA.classList.add('long-quote');
-        quoteTextE.classList.add('long-quote');
-        console.log("long")
-
-    }else {
-        quoteTextA.classList.remove('long-quote');
-        quoteTextE.classList.remove ('long-quote');
-    }
-
-
-
-    quoteTextA.textContent = arabic
-    quoteTextE.textContent = english
-
-    ayahText.textContent = apiQuote['data'][0]['surah']['englishName']+
-    "("+(apiQuote['data'][0]['surah']['number'])+")"+":"+
-    apiQuote['data'][0]['numberInSurah']
-
-    complete();
-
+  complete();
 }
 
-
-function telegram()
-{
-    const teleurl = `https://t.me/share/url?url=${pageurl}&text=${message}`
-    window.open(teleurl,'_blank');
+function telegram() {
+  const teleurl = `https://t.me/share/url?url=${pageurl}&text=${message}`;
+  window.open(teleurl, "_blank");
 }
-
-
-
 
 //Eventlistner
 
-newQuoteBtn.addEventListener('click',newQuote)
-twitterBtn.addEventListener('click',telegram)
-
-
-
+newQuoteBtn.addEventListener("click", getayah);
+twitterBtn.addEventListener("click", telegram);
 
 getayah();
